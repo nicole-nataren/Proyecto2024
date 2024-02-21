2bbbd1cc2f3200367e4a427bc1f3efb1aab1f1bc
Nicole Nataren <nico.nata15@hotmail.com>
2024-02-21 11:02:59 -0600

    Actualización de main
    Se arreglo la función principal main

--- Lista/main.cpp
+++ Lista/main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include "Lista.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main() {
+	
+	cout<<"Te presentamos tu lista sugerida."<<endl;
+	Lista L1;
+	L1.pushFront(1);
+	L1.pushBack(2);
+	L1.pushBack(3);
+	L1.pushBack(4);
+	L1.pushBack(5);
+	cout<<"La lista es: "<<endl;
+	L1.print();
+	cout<<"\nEliminaremos el primer elementos: "<<L1.popFront();
+	cout<<"\nEliminaremos el segundo elementos: "<<L1.popBack();
+	cout<<"\nLa lista es: "<<endl;
+	L1.print();
+	cout<<"\nEl primer elemento es: "<<L1.top();
 
-int main(int argc, char** argv) {
+	
 	return 0;
 }
