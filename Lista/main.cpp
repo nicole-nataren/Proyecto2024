1e33005392bc63b4dc026cdab2ef1b3dac0a4370
Nicole Nataren <90872660+nicole-nataren@users.noreply.github.com>
2024-02-25 20:51:34 -0600

    Arreglos main.cpp

--- Lista/main.cpp
+++ Lista/main.cpp
@@ -1,8 +1,8 @@
+#include <iostream>
 #include "Lista.h"
 
 int main() {
-	
-	cout<<"Te presentamos tu lista sugerida."<<endl;
+
 	Lista L1;
 	L1.pushFront(1);
 	L1.pushBack(2);
@@ -12,11 +12,9 @@ int main() {
 	cout<<"La lista es: "<<endl;
 	L1.print();
 	cout<<"\nEliminaremos el primer elementos: "<<L1.popFront();
-	cout<<"\nEliminaremos el segundo elementos: "<<L1.popBack();
+	cout<<"\nEliminaremos el ultimo elementos: "<<L1.popBack();
 	cout<<"\nLa lista es: "<<endl;
 	L1.print();
-	cout<<"\nEl primer elemento es: "<<L1.top();
-
 	
 	return 0;
 }
