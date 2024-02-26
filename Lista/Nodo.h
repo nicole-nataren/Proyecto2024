7916336931fdfd528e84822ae48bd562310ee653
Nicole Nataren <90872660+nicole-nataren@users.noreply.github.com>
2024-02-25 21:23:14 -0600

    Update Nodo.h

--- Lista/Nodo.h
+++ Lista/Nodo.h
@@ -1,18 +1,12 @@
 #ifndef NODO
 #define NODO
-#include<iostream>
-using std::cin;
-using std::cout;
-using std::endl;
-
 class Nodo{
-private:
-    
+
 public:
+    Nodo();
+    Nodo(int );
     int key;
     Nodo *next;
     Nodo *prev;
-    Nodo();
-    Nodo(int );
 };
 #endif
